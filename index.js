050331f439f37171f8b138726b532ef528d0cfd6
jstirpe <jennstirpe@yahoo.com>
2022-08-06 17:52:17 -0400

    Updated structure, added about section

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const headshot = document.querySelector("#headshot");
-const contact = document.querySelector(".work__contactsList")
 
-headshot.addEventListener("click", () => {
-    contact.classList.toggle("show");
+const contactBtn = document.querySelector(".contact__btn");
+const contactForm = document.querySelector("#contactform");
+const closeBtn = document.querySelector("#close-form");
+
+contactBtn.addEventListener("click", () => {
+    contactForm.classList.add("show-form");
+})
+
+closeBtn.addEventListener("click", () => {
+    contactForm.classList.remove("show-form");
 })
