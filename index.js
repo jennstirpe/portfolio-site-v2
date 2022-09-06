7d50904b96d50db4cbf867ae971190d71fee9963
jstirpe <jennstirpe@yahoo.com>
2022-09-06 15:54:11 -0400

    Updated page structure and styles

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+const openMenuIcon  = document.querySelector(".open");
+const closeMenuIcon  = document.querySelector(".close");
+const navLinks = document.querySelectorAll(".menu__link");
 
-const contactBtn = document.querySelector(".contact__btn");
-const contactForm = document.querySelector("#contactform");
-const closeBtn = document.querySelector("#close-form");
+const menu = document.querySelector(".menu");
 
-contactBtn.addEventListener("click", () => {
-    contactForm.classList.add("show-form");
+// change scroll padding
+const navigationHeight = document.querySelector(".header__nav").offsetHeight;
+document.documentElement.style.setProperty("--scroll-padding", navigationHeight + "px");
+
+// open navigation menu on small screens
+openMenuIcon.addEventListener("click", () => {
+    menu.classList.remove("menu-closed");
+})
+closeMenuIcon.addEventListener("click", () => {
+    menu.classList.add("menu-closed");
 })
 
-closeBtn.addEventListener("click", () => {
-    contactForm.classList.remove("show-form");
+// close navigation if one of the links is selected
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.add("menu-closed");
+    })
 })
+
